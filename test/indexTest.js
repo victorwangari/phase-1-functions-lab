ea635465b1a1f84b4146ecfcbf53a194829d89f6
Victor kamau <Victor Wangari>
2024-03-19 23:01:42 +0300

    initial commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 require ( './helpers.js' );
 
-describe('index.js', function() {
+ describe('index.js', function() {
   describe('distanceFromHqInBlocks()', function() {
     it('returns a distance in blocks', function() {
       expect(distanceFromHqInBlocks(43)).to.equal(1);
